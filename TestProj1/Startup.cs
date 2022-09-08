b4c7daac47e4b410d7b442d495ff1887274a30d4
Monal Patel <Monal Patel@ARMADA60>
2022-09-08 07:25:32 -0400

    Add project files.

---
+++ TestProj1/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestProj1.Startup))]
+namespace TestProj1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+
+        }
+    }
+}
