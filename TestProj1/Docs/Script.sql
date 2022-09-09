2b0a7cc91e2bf313f86d4f46df33b987b206ff11
mpatel <monalp28@yahoo.ca>
2022-09-09 10:51:13 -0400

    Added SQL scripts into Docs folder

---
+++ TestProj1/Docs/Script.sql
@@ -0,0 +1,31 @@
+﻿--SQL scripts example 
+
+--Create table script
+CREATE TABLE [dbo].[AspNetUsers] (
+    [Id]                   NVARCHAR (128) NOT NULL,
+    [Email]                NVARCHAR (256) NULL,
+    [EmailConfirmed]       BIT            NOT NULL,
+    [PasswordHash]         NVARCHAR (MAX) NULL,
+    [SecurityStamp]        NVARCHAR (MAX) NULL,
+    [PhoneNumber]          NVARCHAR (MAX) NULL,
+    [PhoneNumberConfirmed] BIT            NOT NULL,
+    [TwoFactorEnabled]     BIT            NOT NULL,
+    [LockoutEndDateUtc]    DATETIME       NULL,
+    [LockoutEnabled]       BIT            NOT NULL,
+    [AccessFailedCount]    INT            NOT NULL,
+    [UserName]             NVARCHAR (256) NOT NULL,
+    CONSTRAINT [PK_dbo.AspNetUsers] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [UserNameIndex]
+    ON [dbo].[AspNetUsers]([UserName] ASC);
+
+--Insert script
+INSERT INTO [dbo].[AspNetUsers] ([Id], [Email], [EmailConfirmed], [PasswordHash], [SecurityStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEndDateUtc], [LockoutEnabled], [AccessFailedCount], [UserName]) VALUES (N'09945f01-517b-41a7-9228-9ffd3c29a873', N'mpatel3@armadadata.com', 0, N'ALggZoBEZCPG399Mq6EqYdNCyjcgFjHwf/MM5a230u5I76hghEAqfjBVozb+yZY4pw==', N'40768475-47ea-4461-8fdb-357701ef929f', NULL, 0, 0, NULL, 1, 0, N'mpatel3@armadadata.com')
+
+
+-- Select script
+SELECT TOP 10 [Id] ,[Email] ,[EmailConfirmed] ,[PasswordHash] ,[SecurityStamp] ,[PhoneNumber] ,[PhoneNumberConfirmed] ,[TwoFactorEnabled] ,[LockoutEndDateUtc] ,[LockoutEnabled] ,[AccessFailedCount] ,[UserName] FROM [dbo].[AspNetUsers]
+
